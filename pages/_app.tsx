68c7777ea3c585daf586d57b0d2db2521ee784fb
hao <wongyh@uw.edu>
2023-01-03 23:12:43 -0800

    add audience analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { Analytics } from '@vercel/analytics/react';
 config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <>
         <Component {...pageProps} />
+          <Analytics/>
       </>)
 
 }
